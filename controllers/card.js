1f0aa4f464fde9cd05ae3e8e6887f06a483d3556
Timophey Shalonsky <shalonsky.tim@yandex.ru>
2022-10-20 00:49:42 +0300

    hotfixes: card-id error

--- controllers/card.js
+++ controllers/card.js
@@ -35,54 +35,72 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.likeCard = (req, res) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
-    { new: true },
-  )
-    .then(card => res.send({ data: card }))
-    .catch((err) => {
-      if (err.name === 'NotFoundError') {
-        return res.status(404).send({
-          "message": "Запрашиваемый объект не найден"
-        });
-      }
-      res.status(500);
-      console.log(`Произошла неизвестная ошибка ${err.name}: ${err.message}`);
-      return;
+  if (req.params.userId.match(/^[0-9a-fA-F]{24}$/)) {
+    Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    )
+      .then(card => res.send({ data: card }))
+      .catch((err) => {
+        if (err.name === 'NotFoundError') {
+          return res.status(404).send({
+            "message": "Запрашиваемый объект не найден"
+          });
+        }
+        res.status(500);
+        console.log(`Произошла неизвестная ошибка ${err.name}: ${err.message}`);
+        return;
+      });
+  } else {
+    return res.status(400).send({
+      "message": "Переданы некорректный идентификатор карточки"
     });
+  }
 };
 
 module.exports.removeLike = (req, res) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } },
-    { new: true },
-  )
-    .then(card => res.send({ data: card }))
-    .catch((err) => {
-      if (err.name === 'NotFoundError') {
-        return res.status(404).send({
-          "message": "Запрашиваемый объект не найден"
-        });
-      }
-      res.status(500);
-      console.log(`Произошла неизвестная ошибка ${err.name}: ${err.message}`);
-      return;
+  if (req.params.userId.match(/^[0-9a-fA-F]{24}$/)) {
+    Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true },
+    )
+      .then(card => res.send({ data: card }))
+      .catch((err) => {
+        if (err.name === 'NotFoundError') {
+          return res.status(404).send({
+            "message": "Запрашиваемый объект не найден"
+          });
+        }
+        res.status(500);
+        console.log(`Произошла неизвестная ошибка ${err.name}: ${err.message}`);
+        return;
+      });
+  } else {
+    return res.status(400).send({
+      "message": "Переданы некорректный идентификатор карточки"
     });
+  }
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId)
-    .then(cards => res.send({ data: cards }))
-    .catch((err) => {
-      if (err.name === 'NotFoundError') {
-        return res.status(404).send({
-          "message": "Запрашиваемый объект не найден"
-        });
-      }
-      res.status(500);
-      console.log(`Произошла неизвестная ошибка ${err.name}: ${err.message}`);
-      return;
+  if (req.params.userId.match(/^[0-9a-fA-F]{24}$/)) {
+    Card.findByIdAndRemove(req.params.cardId)
+      .then(cards => res.send({ data: cards }))
+      .catch((err) => {
+        if (err.name === 'NotFoundError') {
+          return res.status(404).send({
+            "message": "Запрашиваемый объект не найден"
+          });
+        }
+        res.status(500);
+        console.log(`Произошла неизвестная ошибка ${err.name}: ${err.message}`);
+        return;
+      });
+  } else {
+    return res.status(400).send({
+      "message": "Переданы некорректный идентификатор карточки"
     });
+  }
 }
